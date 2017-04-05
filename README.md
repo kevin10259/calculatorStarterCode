715bb45db1ae82ded5b462c85fd2aebf84a8736c
Kevin Moposita <kmoposita@aclanyc.com>
2017-04-05 18:29:31 +0000

    Change README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # calculatorStarterCode
+
+What this program does is calculate any two numbers using mathematical operations.
+
+## How this project functions
+This program functions by executing the desired function the user picks and implements this with two numbers.
+
+## What I'd add 
+What I would add to this project is more functions that a user can use.
--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -18,30 +18,30 @@ function multiply(x,y)
 
 function subtract(x,y)
 {
-	
+	return x - y;
 }
 
 function add(x,y)
 {
-	
+	return x + y;
 }
 
 function power(x,y)
 {
-	
+	return Math.pow(x,y);
 }
 
 function square_root(x)
 {
-	
+	return Math.sqrt(x);
 }
 
 function logBase10(x)
 {
-	
+	return Math.log10(x);
 }
 
 function generateRandomNumber()
 {
-	
+	return Math.random();
 }
